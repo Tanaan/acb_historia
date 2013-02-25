2c149374b29886b6694814d2a638d2f74c0e8dce
Tanaan <juan.matarramz@gmail.com>
2013-02-25 23:40:30 +0100

    Mi primer commit chispas.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AcbHistoria::Application.config.secret_token = '94f0d260fa5352b98fd45d15a5e0d734a298cff032cb0ef6010fcbffa6e25ffe464d520082960f0414851e1308b9083666b53c54ad7f74ad22f8c92cada2d0b5'
